41c80a808331ba9db9a6b1eb166e49661e69692f
Wouter Dijkstra <wouter.dijkstra.wd@gmail.com>
2019-01-15 21:20:22 +0100

    Update 2.1.py
    Changed ** to * only

--- Week_2/2.1.py
+++ Week_2/2.1.py
@@ -1,11 +1,4 @@
 """
-Student Naam:   Wouter Dijkstra
-Student Nr. :   1700101
-Klas        :   ??
-Docent      :   frits.dannenberg@hu.nl
-"""
-
-"""
 Description
 -----------
 A function to perform power calculations with a base and exponent
@@ -23,25 +16,22 @@ Return
   : Integer
     returns the result of the calculation
 """
-
-
 def power(base, exponent):
     assert exponent > 0  # Assert to see if the exponent is larger than or equal to 1
+    count = 0
     result = 1
     while exponent > 0:
-        if exponent % 2 == 0:  # Check if the exponent is equal
-            base = base * base  # base squared 2
-            exponent //= 2  # Divide the exponent by 2
-            result = base
-
-            if exponent == 1:  # If the exponent is 1, return because it's already the correct answer
-                return result
+        count += 1
+        if exponent % 2 == 0: # Check if the exponent is equal
+            base *= base # base squared 2
+            exponent /= 2 # Divide the exponent by 2
         else:
             exponent -= 1
-            return base * (base ** exponent)
+            result *= base
+
+    print("Times multiplied:", count)
     return result
 
 # Main
-
 print(power(2, 16))
-
+print(power(2, 49))
